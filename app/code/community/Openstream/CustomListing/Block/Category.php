77173967bd0b6ee6f2d4b08ee9ebf5a65a92f97f
Nick de Kleijn <nick@h-o.nl>
2016-01-19 11:11:36 +0100

    HACO-137: added getCategory function to enable getting category object for URL retrieval on Magento Enterprise

--- app/code/community/Openstream/CustomListing/Block/Category.php
+++ app/code/community/Openstream/CustomListing/Block/Category.php
@@ -2,23 +2,37 @@
 
 class Openstream_CustomListing_Block_Category extends Openstream_CustomListing_Block_Abstract
 {
+
+    protected $_category;
+
+
+    /**
+     * @return Mage_Catalog_Model_Category
+     */
+    public function getCategory()
+    {
+        return $this->_category;
+    }
+
+
     protected function _getProductCollection()
     {
         if ($this->getIdPath()) {
             $categoryIdPath = explode('/',$this->getIdPath());
             $this->setCategoryId($categoryIdPath[1]);
+            $this->_category = Mage::getModel('catalog/category')->load($this->getCategoryId());
         }
 
         if (is_null($this->_productCollection)
-            && $category = Mage::getModel('catalog/category')->load($this->getCategoryId())) {
+            && $this->_category) {
 
             $this->_productCollection = Mage::getResourceModel('reports/product_collection');
-            $this->_productCollection->addCategoryFilter($category)
+            $this->_productCollection->addCategoryFilter($this->_category)
                                      ->addAttributeToSelect('*')
                                      ->addStoreFilter()
                                      ->addPriceData();
 
-            $this->prepareSortableFieldsByCategory($category);
+            $this->prepareSortableFieldsByCategory($this->_category);
 
             if (! Mage::helper('cataloginventory')->isShowOutOfStock()) {
                 Mage::getSingleton('cataloginventory/stock')->addInStockFilterToCollection($this->_productCollection);
